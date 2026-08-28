410a6dcf1713ed6d6906783fce554415712febda
bcastets-robotiq <b.castets@robotiq.com>
2026-08-28 14:57:35 -0400

    Minimal example
    Add a wait for echo

--- sdk_cpp/examples/quick_start.cpp
+++ sdk_cpp/examples/quick_start.cpp
@@ -7,11 +7,9 @@
 using namespace std::chrono_literals; // enables the 1s / 200ms / 5s literals below
 
 int main() {
-    std::cout << "Gripper test" << std::endl;
-
     // 1- Create the connection config
     Robotiq::ConnectionConfig config;
-    config.serial.port = "COM4";
+    config.serial.port = "COM4"; //or "/dev/ttyUSB0" for linux. Adjust the port name according to your system.
 
     // 2- Create the gripper object
     Robotiq::Gripper gripper = Robotiq::Gripper(config);
@@ -23,14 +21,19 @@ int main() {
     Robotiq::GripperCommand command = Robotiq::GripperCommand::defaults();
     command.action.set(Robotiq::ActionRequestBit::GoTo);
     command.positionRequest = 100;
+    command.speed = 255;
+    command.force = 255;
 
     // 5- Set command
     gripper.setCommand(command);
 
-    // 6- Wait for the action to complete
+    // 6- Wait for the gripper to echo
+    bool positionRequestEchoed = Robotiq::waitFor([&]{return gripper.getStatus().positionRequestEcho == command.positionRequest;},1s);
+
+    // 7- Wait for the action to complete
     bool motionCompleted = Robotiq::waitFor([&]{return (gripper.getStatus().gripperStatus.objectDetection() != Robotiq::ObjectDetection::Moving);},10s);
 
-    // 7- retrieve status
+    // 8- retrieve status
     uint8_t currentPosition = gripper.getStatus().position;
 
     // Print retrieved status
